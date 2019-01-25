aa55391ee9451c455a860b1915f1bbd93e643b0a
Joshua MacDonald <jmacd@users.noreply.github.com>
2019-01-25 12:49:52 -0800

    Add USER and PROJECT_ID flags LS-6796 (#1)
    * Add three environment variables for dev-mode

--- lightstep-developer-satellite.sh
+++ lightstep-developer-satellite.sh
@@ -21,9 +21,25 @@ if [ -z "$LIGHTSTEP_API_KEY" ]; then
   echo "Thank you. In the future you may set LIGHTSTEP_API_KEY to skip this step."
 fi
 
+if [ -z "$LIGHTSTEP_USER" ]; then 
+  echo "LIGHTSTEP_USER is not set.  This is the e-mail address you use for sign-in."
+  echo "Please enter your user name:"
+  read -r LIGHTSTEP_USER
+  echo "Thank you. In the future you may set LIGHTSTEP_USER to skip this step."
+fi
+
+if [ -z "$LIGHTSTEP_PROJECT_ID" ]; then 
+  echo "LIGHTSTEP_PROJECT_ID is not set.  This must be set on the command-line."
+  exit 1
+fi
+
 ## Set env vars to be passed to docker if not set in current environment.
 COLLECTOR_API_KEY="$LIGHTSTEP_API_KEY"
-: "${COLLECTOR_POOL:=${USER}_developer_pool}"
+# Developer-mode specifics
+: "${COLLECTOR_POOL:=${LIGHTSTEP_USER}_developer_pool}"
+: "${COLLECTOR_INGESTION_TAGS:=lightstep.developer:${LIGHTSTEP_USER}}"
+: "${COLLECTOR_DISABLE_ACCESS_TOKEN_CHECKING:=true}"
+: "${COLLECTOR_PROJECT_ID:=${LIGHTSTEP_PROJECT_ID}}"
 # Set default ports
 : "${COLLECTOR_BABYSITTER_PORT:=8000}"
 : "${COLLECTOR_ADMIN_PLAIN_PORT:=8080}"
@@ -39,7 +55,6 @@ COLLECTOR_API_KEY="$LIGHTSTEP_API_KEY"
 docker pull lightstep/collector
 
 docker run \
-  -d \
   -e COLLECTOR_API_KEY="$COLLECTOR_API_KEY" \
   -e COLLECTOR_POOL="$COLLECTOR_POOL" \
   -e COLLECTOR_BABYSITTER_PORT="$COLLECTOR_BABYSITTER_PORT"  -p "$COLLECTOR_BABYSITTER_PORT":"$COLLECTOR_BABYSITTER_PORT" \
@@ -48,5 +63,7 @@ docker run \
   -e COLLECTOR_GRPC_PLAIN_PORT="$COLLECTOR_GRPC_PLAIN_PORT"  -p "$COLLECTOR_GRPC_PLAIN_PORT":"$COLLECTOR_GRPC_PLAIN_PORT" \
   -e COLLECTOR_PLAIN_PORT="$COLLECTOR_PLAIN_PORT"  -p "$COLLECTOR_PLAIN_PORT":"$COLLECTOR_PLAIN_PORT" \
   -e COLLECTOR_REPORTER_BYTES_PER_PROJECT="$COLLECTOR_REPORTER_BYTES_PER_PROJECT" \
-  --restart always \
+  -e COLLECTOR_INGESTION_TAGS="$COLLECTOR_INGESTION_TAGS" \
+  -e COLLECTOR_DISABLE_ACCESS_TOKEN_CHECKING="$COLLECTOR_DISABLE_ACCESS_TOKEN_CHECKING" \
+  -e COLLECTOR_PROJECT_ID="$COLLECTOR_PROJECT_ID" \
   lightstep/collector
