ae514ba69c5e666cc170a53a72bc8dffedaf0493
Joshua MacDonald <jmacd@users.noreply.github.com>
2019-02-14 13:03:44 -0800

    Allow forwarded tags (#8)

--- stop-developer-satellite.sh
+++ stop-developer-satellite.sh
@@ -3,11 +3,11 @@
 NAME=lightstep_developer_satellite
 
 while true; do
-  ID=$(docker ps | grep ${NAME} | head -n 1 | cut -d ' ' -f 1 )
+  ID=$(docker ps --all | grep ${NAME} | head -n 1 | cut -d ' ' -f 1 )
   if [ -n "$ID" ]; then
     echo "Removing docker container $ID"
-    docker kill "$ID"
-    docker rm "$ID"
+    docker kill "$ID" || true
+    docker rm "$ID" || true
   else
     break
   fi
