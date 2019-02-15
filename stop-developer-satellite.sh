a45de305e60d4552ef4979c19ef77171dcf55bb8
jmacd <jmacd@lightstep.com>
2019-02-14 21:58:21 -0800

    Suppress stderr

--- stop-developer-satellite.sh
+++ stop-developer-satellite.sh
@@ -6,8 +6,8 @@ while true; do
   ID=$(docker ps --all | grep ${NAME} | head -n 1 | cut -d ' ' -f 1 )
   if [ -n "$ID" ]; then
     echo "Removing docker container $ID"
-    docker kill "$ID" || true
-    docker rm "$ID" || true
+    docker kill "$ID" > /dev/null 2>&1 || true
+    docker rm "$ID" > /dev/null 2>&1 || true
   else
     break
   fi
